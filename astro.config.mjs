3052aa5a9b75821624eacd52bccf5b270b85c0e0
dmtrbch <bochvarovskidimitar@gmail.com>
2025-09-04 15:12:11 +0200

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dmtrbch.github.io',
-  output: '/stoken-demo',
+  base: '/stoken-demo',
 });
